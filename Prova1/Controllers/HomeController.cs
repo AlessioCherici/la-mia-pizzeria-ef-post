69c0edfaaf889bab8c5dd92f329d4a891af75cde
AlessioCherici <alessio.cherici@hotmail.it>
2023-01-12 17:23:52 +0100

    Aggiungere i file di progetto.

---
+++ Prova1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPizzeriaModel.Models;
+using System.Diagnostics;
+
+
+namespace MyPizzeriaModel.Controllers
+    {
+    public class HomeController : Controller
+        {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+            {
+            _logger = logger;
+            }
+
+        public IActionResult Index()
+            {
+            using PizzaContext db = new PizzaContext();
+            List<Pizza> listaPizze = db.Pizzas.ToList();
+            return View(listaPizze);
+            }
+
+        public IActionResult Dettagli(int id)
+            {
+            using PizzaContext db = new PizzaContext();
+            Pizza pizza = (from p in db.Pizzas where p.Id == id select p).FirstOrDefault();
+            if (pizza == null)
+                {
+                return NotFound();
+                }
+            else
+                {
+                return View(pizza);
+                }
+            }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+            {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+
+        }
+    }
---
+++ Prova1/Models/Pizza.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyPizzeriaModel.Models
+    {
+    [Table("Pizzas")]
+    public class Pizza
+        {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Prezzo { get; set; }
+        public string Descrizione { get; set; }
+        public string Immagine { get; set; }
+
+        public Pizza (string nome, string prezzo, string descrizione, string Immagine)
+            {
+            this.Nome = nome;
+            this.Prezzo = prezzo;
+            this.Descrizione = descrizione;
+            this.Immagine = Immagine;
+            }
+        }
+    }
